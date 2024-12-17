4362482fbac5876519156eb9dc0ff412f2239bac
x3nom <jakub.p12345678910@gmail.com>
2024-12-17 18:00:49 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ensure python dependencies are installed
+pip3 install -r requirements.txt
+
+
+
 TARGET_DIR="$HOME/.local/bin"
 
 SRC_PATH="./src/testcase-tools.py"
@@ -15,6 +20,8 @@ cp "$SRC_PATH" "$TARGET_DIR/$NAME"
 # Ensure the script is executable
 chmod +x "$TARGET_DIR/$NAME"
 
-echo "Installation complete! Make sure $TARGET_DIR is in your PATH."
+
+printf "\n\033[1;32mInstallation complete!\033[0m\n"
+echo "Make sure $TARGET_DIR is in your PATH."
 echo "If it's not, add following to your .bashrc/.zshrc:"
-echo "  export PATH=\"\$HOME/.local/bin:\$PATH\""
+printf "\033[3mexport PATH=\"$TARGET_DIR:\$PATH\"\033[0m\n"
